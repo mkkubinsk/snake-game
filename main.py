e009073e5864bdfe386399b6337139e8b95ab471
mkkubinsk <mk.kubinski@gmail.com>
2023-07-12 23:15:59 +0200

    Food added

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
+# Screen configuration
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
 screen.tracer(0)
 
+# Snake creation
 snake = Snake()
+food = Food()
 
+# Snake navigation based on key-presses
 screen.listen()
 screen.onkey(key='Up', fun=snake.turn_up)
 screen.onkey(key='Down', fun=snake.turn_down)
 screen.onkey(key='Left', fun=snake.turn_left)
 screen.onkey(key='Right', fun=snake.turn_right)
 
+# Game loop
 game_on = True
-
 while game_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.restart()
 
 
 screen.exitonclick()
